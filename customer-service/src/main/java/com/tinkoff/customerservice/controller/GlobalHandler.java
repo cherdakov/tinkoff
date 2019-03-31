de003e316f584d7376495bb199ed78fe710facfc
cherdakov <eqx11@yandex.ru>
2019-03-31 14:21:32 +0700

    Fix global handler return values

--- customer-service/src/main/java/com/tinkoff/customerservice/controller/GlobalHandler.java
+++ customer-service/src/main/java/com/tinkoff/customerservice/controller/GlobalHandler.java
@@ -5,17 +5,20 @@ import entity.ResponseData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalHandler {
     @ExceptionHandler(CustomerServiceException.class)
+    @ResponseBody
     ResponseData handleException(CustomerServiceException e){
         log.error("", e);
         return new ResponseData<>(ResponseData.ResultCode.ERROR, e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
+    @ResponseBody
     ResponseData handleException(Exception e){
         log.error("", e);
         return new ResponseData<>(ResponseData.ResultCode.ERROR, e.getMessage());
